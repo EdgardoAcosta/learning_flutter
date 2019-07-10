5891d9c2c2ad1f88ebfd5d194a94758fc8c84ac3
EdgardoAcosta <edgardo_acostaleal@hotmail.com>
2019-07-10 15:43:26 -0500

    Finish quizz exmple

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
+QuizBrain quizBrain = QuizBrain();
 
 class Quizzler extends StatelessWidget {
   @override
@@ -25,6 +28,42 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  int questionIndex = 0;
+
+  void changeQuestion({bool incomingButton}) {
+    setState(() {
+      if (!quizBrain.isFinished()) {
+        bool correctAnswer = quizBrain.getQuestionAnswer();
+
+        if (correctAnswer == incomingButton) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+
+        quizBrain.nextQuestin();
+      } else {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        quizBrain.reset();
+
+        scoreKeeper = [];
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +76,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +100,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                changeQuestion(incomingButton: true);
               },
             ),
           ),
@@ -79,19 +118,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                changeQuestion(incomingButton: false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ quizzler-flutter/lib/question.dart
@@ -0,0 +1,14 @@
+
+
+class Question extends Object {
+  String questionText;
+  bool questionAnswer;
+
+  Question(String q, bool a) {
+    questionText = q;
+    questionAnswer = a;
+  }
+
+
+
+}
