c81ec10a33dbe3107ee092848d64b7a49a0fb9c2
EdgardoAcosta <edgardo_acostaleal@hotmail.com>
2019-10-15 22:15:13 -0500

    Update

--- emailapp/lib/screens/contacts_screen.dart
+++ emailapp/lib/screens/contacts_screen.dart
@@ -1,10 +1,13 @@
 import 'package:emailapp/models/contact.dart';
 import 'package:emailapp/services/contact_manager.dart';
 import 'package:emailapp/widgets/app_drawer.dart';
+import 'package:emailapp/widgets/contact_counter.dart';
 import 'package:emailapp/widgets/contact_list_builder.dart';
 import 'package:emailapp/widgets/contact_search.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../overseer.dart';
+import '../provider.dart';
 
 class ContactsScreen extends StatefulWidget {
   @override
@@ -12,28 +15,14 @@ class ContactsScreen extends StatefulWidget {
 }
 
 class _ContactsScreenState extends State<ContactsScreen> {
-  ContactManager manager = ContactManager();
-
   @override
   Widget build(BuildContext context) {
+    ContactManager manager = Provider.of(context).fetch(ContactManager);
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
         actions: <Widget>[
-          Chip(
-            label: StreamBuilder<int>(
-                stream: manager.contactCounter,
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  return Text(
-                    (snapshot.data ?? 0).toString(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                }),
-            backgroundColor: Colors.red,
-          ),
+          ContactCounter(),
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
